1008ebb24cc4fee88cd17b26308db4736e5261e5
ribhy-bishtawi <ribhy2001@gmail.com>
2022-09-14 11:21:29 +0300

    Role base
    Only the admin can view the all page and the users can only view the orders page

--- account/decorators.py
+++ account/decorators.py
@@ -1,3 +1,4 @@
+from tokenize import group
 from django.http import HttpResponse
 from django.shortcuts import redirect
 
@@ -9,3 +10,15 @@ def unauthenticatedUser(viewFun):
         else:
             return viewFun(request, *args, **kwargs)
     return wrapperFun
+
+
+def allowedUsers(viewFun):
+    def wrapper_fun(request, *args, **kwargs):
+        group = None
+        if(request.user.groups.exists()):
+            group = request.user.groups.all()[0].name
+        if group == 'admin':
+            return viewFun(request, *args, **kwargs)
+        else:
+            return redirect('orders')
+    return wrapper_fun

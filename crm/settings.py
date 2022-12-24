2e5e2da1aed08d67fa476b742a585a2d4dfcfc91
ribhy-bishtawi <ribhy2001@gmail.com>
2022-12-25 00:00:36 +0300

    sh file

--- crm/settings.py
+++ crm/settings.py
@@ -159,7 +159,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", 'staticfiles')
 STATIC_URL = 'static/'
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'

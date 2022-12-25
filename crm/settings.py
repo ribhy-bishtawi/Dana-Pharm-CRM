f9d0d14f36d10ab4c86de29cb71bbb6be78f4a18
ribhy-bishtawi <ribhy2001@gmail.com>
2022-12-25 11:02:29 +0300

    sh file

--- crm/settings.py
+++ crm/settings.py
@@ -167,7 +167,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-
+DATABASES = {}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field

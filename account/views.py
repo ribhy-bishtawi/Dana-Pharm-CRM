3dda236aaed64dc3fd4925fd7b0ce463d9ecef1c
ribhy-bishtawi <ribhy2001@gmail.com>
2023-05-03 14:48:20 +0300

    Remove case sensitivity

--- account/views.py
+++ account/views.py
@@ -200,7 +200,8 @@ def loginPage(request):
         username = request.POST.get('username')
         password = request.POST.get('password')
 
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(request, username=username.lower(),
+                            password=password.lower())
 
         if user is not None:
             login(request, user)
